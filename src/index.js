de592b83c3c9ca685bec20a397f95c022dde0011
rnwathon <rakhmatnashrul@gmail.com>
2022-07-01 11:19:01 +0700

    feat: update app & index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
